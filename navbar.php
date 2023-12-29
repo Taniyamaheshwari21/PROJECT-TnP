cc80dbe1b6c8fe52083e01c7b4ea54525377a9e3
Taniyamaheshwari21 <133154389+Taniyamaheshwari21@users.noreply.github.com>
2023-12-29 20:32:57 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar  navbar-expand-lg navbar-dark ">
+  <a class="navbar-brand" href="/dbms">Training and Placement</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <a href="/dbms/home.php"><img src="logo.png"/></a>
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="logo">
+    </div>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link bro" href="/dbms/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dbms/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/dbms/logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
---
+++ ugh.js
@@ -0,0 +1,16 @@
+
+// var count=1;
+const btn=document.getElementById("woo");
+//count++;
+function clickEvent() {  
+    //if(count==1){
+        window.alert("You have successfully applied to this company.More details will be shared to you via SMS or mail to you.")
+        //const get=document.getElementById('btn');
+        //get.click();
+        //count++; 
+//     }
+//     else{
+//         window.alert("You have already applied to this company");
+//     } 
+} 
+btn.addEventListener("click",clickEvent());
